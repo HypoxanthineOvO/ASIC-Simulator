5806970bfc6f4917af4519c02868a1d8a342b32a
HypoxanthineOvO <hyx021203@163.com>
2024-10-08 22:10:29 +0800

    Add Framework of Cache

--- MemorySystem/cache.hpp
+++ MemorySystem/cache.hpp
@@ -3,11 +3,48 @@
 
 #include "dram.hpp"
 
-class Cache {
+typedef enum EvictionPolicy {
+    RANDOM,
+    FIFO,
+    LRU,
+} EvictionPolicy;
+
+class CacheEntry {
 public:
 
 private:
+    // Data
+    uint32_t cache_line_size;
+    std::vector<uint8_t> data;
+
+    // Metadata
+    uint32_t tag;
+    bool valid;
+    bool dirty;
+
+};
+
+class Cache {
+public:
+    Cache(uint32_t cache_size, uint32_t num_associativity = -1, EvictionPolicy policy = RANDOM): size(cache_size) {
+        if (num_associativity == -1) {
+            // Fully associative
+            num_associativity = size;
+        }
+        num_groups = size / num_associativity;
+    }
+private:
+    // Addressing Function
+
+
     std::shared_ptr<DRAM> dram;
+    EvictionPolicy policy;
+
+    uint32_t size;
+    uint32_t cacheline_size;
+    std::vector<CacheEntry> cache;
+    uint32_t num_associativity;
+    uint32_t num_groups;
 };
 
 
